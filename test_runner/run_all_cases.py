42a71e59952d00fc970300bb494879bb1f6f7799
liguanzhuo <1971996280@qq.com>
2021-01-07 17:06:06 +0800

    跑通参数化

--- test_runner/run_all_cases.py
+++ test_runner/run_all_cases.py
@@ -3,8 +3,9 @@ import unittest
 import os
 from utils import HTMLTestReportCN
 from utils.config_utils import local_config
-casepath = os.path.join(os.path.dirname(__file__), '..','testcases')#测试报告路径
-report=os.path.join(os.path.dirname(__file__),'..',local_config.REPORT_PATH)
+casepath = os.path.join(os.path.dirname(__file__), '..','testcase')#测试报告路径
+result_path=os.path.join(os.path.dirname(__file__),'..',local_config.REPORT_PATH)
+
 def load_testcase():
     discover = unittest.defaultTestLoader.discover(start_dir=casepath,
                                                        pattern='test_api_case.py',
@@ -14,14 +15,14 @@ def load_testcase():
     return all_case_suite
 
 
-report_dir = HTMLTestReportCN.ReportDirectory(report)  # 创建一个测试报告路径对象
-report_dir.create_dir('test_framwork')  # 调用创建目录方法
-report_html_path = HTMLTestReportCN.GlobalMsg.get_value('report_path')  # 获取测试报告文件的路径 此处必须填reportpath
-report_html_file = open(report_html_path, 'wb')
-html_runner = HTMLTestReportCN.HTMLTestRunner(stream=report_html_file,
+result_dir = HTMLTestReportCN.ReportDirectory(result_path)  # 创建一个测试报告路径对象
+result_dir.create_dir('test_framwork')  # 调用创建目录方法
+report_html_path = HTMLTestReportCN.GlobalMsg.get_value('report_path')  # 获取测试报告文件的路径 此处必须填report_path
+report_html_obj = open(report_html_path, 'wb')
+html_runner = HTMLTestReportCN.HTMLTestRunner(stream=report_html_obj,
                                                       title='接口测试报告',
                                                       description='数据驱动和关键字驱动',
                                                       tester='Liiigz'
                                                       )
 html_runner.run(load_testcase())
-report_html_file.close()
+report_html_obj.close()
